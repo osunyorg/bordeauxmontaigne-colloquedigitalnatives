e4adca22918a72a1ba0f98e25bdb6525692ab457
Arnaud Levy <contact@arnaudlevy.com>
2023-10-06 10:26:58 +0200

    package

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-var devPlugins = {},
-    productionPlugins = {
-        autoprefixer: {}
-    };
-
-module.exports = {
-    plugins: process.env.HUGO_ENVIRONMENT === 'production' ? productionPlugins : devPlugins
-};
